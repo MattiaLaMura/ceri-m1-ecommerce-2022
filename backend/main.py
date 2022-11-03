c7efa436c26388c7d01cb6ecf48509115f033493
LorenzoLaMura <71428385+LorenzoLaMura@users.noreply.github.com>
2022-11-03 19:41:17 +0100

    Revert "Merge Back end"

--- backend/main.py
+++
@@ -1,6 +0,0 @@
-""" This file is used to launch the Backend. """
-import uvicorn
-from src.run_backend import app
-
-if __name__ == '__main__':
-    uvicorn.run(app)
--- backend/setup.py
+++
@@ -1,4 +0,0 @@
-""" Install and generate packages. """
-from setuptools import setup, find_packages
-
-setup(name='generator', packages=find_packages())
