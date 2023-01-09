3876ab705bb52a8b08da125894e54a0d72580444
MattiaLaMura <mattia.la-mura@alumni.univ-avignon.fr>
2023-01-09 15:16:02 +0100

    fix unit test algolia

--- backend/src/classes/algolia.py
+++ backend/src/classes/algolia.py
@@ -4,13 +4,17 @@ from src.classes.artist import get_all_artists
 from src.classes.album import get_albums_from_artist
 from src.classes.song import get_songs_from_album
 
-client = SearchClient.create(Settings().dict()['algolia_application_id'],
-                             Settings().dict()['algolia_api_key'])
-index = client.init_index(Settings().dict()['algolia_index_name'])
+
+def get_vars():
+    client = SearchClient.create(Settings().dict()['algolia_application_id'],
+                                Settings().dict()['algolia_api_key'])
+    index = client.init_index(Settings().dict()['algolia_index_name'])
+    return index
 
 
 def index_catalog():
     """ This method uses Algolia to index the catalog. """
+    index = get_vars()
     artists = get_all_artists()
     for artist in artists:
         albums = get_albums_from_artist(artist.artist_id)
@@ -32,4 +36,5 @@ def index_catalog():
 
 def search_engine(word_searched: str):
     """ This method uses Algolia to provide a search engine. """
+    index = get_vars()
     return index.search(word_searched, {"hitsPerPage": 5})
